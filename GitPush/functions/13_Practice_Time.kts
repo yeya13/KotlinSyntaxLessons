12a0f7f75cdc3b639829c1f484a95b87392b3c59
Claudia <clau13052002@gmail.com>
2022-09-01 15:15:58 -0500

    add quiz practice time 15 and modified the practice 13

--- GitPush/functions/13_Practice_Time.kts
+++ GitPush/functions/13_Practice_Time.kts
@@ -2,11 +2,15 @@ fun main(){
     println(whatShouldIDoToday("happy"))
 }
 
-fun whatShouldIDoToday(mood: String, weather: String="Sunny", temperature: Int = 24): String{
+fun whatShouldIDoToday(mood: String, weather: String="sunny", temperature: Int = 24): String{
     return when {
-        mood == "happy" && weather == "Sunny" -> "go for a walk"
+        mood == "happy" && weather == "sunny" -> "go for a walk"
+        mood == "angry" && wather == "cloudy" -> "drink hot chocolate"
+        mood == "sad" && weather == "rainy" && temperature == 0 -> "stay in bed"
+        temperature > 35 -> "go swimming"
         else -> "Stay home and read."
     }
 }
+
 
 main()
